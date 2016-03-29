cdb3fac7f796a4eacf72b5d8074eb61dd7782483
tom wiss <tom@robotican.net>
2016-03-29 18:39:54 +0300

    update

--- robotican_installation/third_pkg_setup/setup.sh
+++ robotican_installation/third_pkg_setup/setup.sh
@@ -12,15 +12,15 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo -e "\e[34mInstalling additional resources...\e[0m"
 apt-get -y install chrony openssh-server expect espeak ros-indigo-joy ros-indigo-hector-slam ros-indigo-usb-cam ros-indigo-openni2* ros-indigo-hokuyo-node ros-indigo-dynamixel-motor python-pygame ros-indigo-yocs-velocity-smoother
-apt-get -y install ros-indigo-moveit-full ros-indigo-ueye-cam
+apt-get -y install ros-indigo-moveit-full ros-indigo-ueye-cam ros-indigo-gazebo-ros-pkgs ros-indigo-gazebo-ros-control ros-indigo-hector-gazebo ros-indigo-ros-controllers
 echo -e "\e[34mSyncing clock...\e[0m"
 ntpdate ntp.ubuntu.com
 
-echo -e "\e[34mSetting usb rules..\e[0m"
-cp $DIR/ric_usb.rules /etc/udev/rules.d
-echo -e "\e[34mRestarting rules..\e[0m"
-/etc/init.d/udev reload
-echo -e "\e[34mInstallation is complete..\e[0m"
+#echo -e "\e[34mSetting usb rules..\e[0m"
+#cp $DIR/ric_usb.rules /etc/udev/rules.d
+#echo -e "\e[34mRestarting rules..\e[0m"
+#/etc/init.d/udev reload
+#echo -e "\e[34mInstallation is complete..\e[0m"
 
 
 
