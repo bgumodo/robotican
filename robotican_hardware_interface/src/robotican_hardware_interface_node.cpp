bb1d5268ae791f67915541f43b862387d0c23abb
tom wiss <tom@robotican.net>
2016-04-17 16:42:11 +0300

    [UPDATE robotican_hardware_interface]: Add parameter robot_type to determinant the robot type

--- robotican_hardware_interface/src/robotican_hardware_interface_node.cpp
+++ robotican_hardware_interface/src/robotican_hardware_interface_node.cpp
@@ -16,9 +16,11 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "robotican_hardware_interface_node");
     ros::NodeHandle nodeHandle;
     bool isLizi = false, isArmadilo = false, isKomodo = false;
+    std::string robotType;                                      /* determent the robot type, e.g default. */
     ros::param::param("komodo", isKomodo, false);
     ros::param::param("armadilo", isArmadilo, false);
     ros::param::param("lizi", isLizi, false);
+    ros::param::param("robot_type", robotType, "default");
     ros::Duration(1.0).sleep();
 
     if(isLizi && isArmadilo || isLizi && isKomodo || isArmadilo && isKomodo) {
@@ -27,45 +29,51 @@ int main(int argc, char **argv) {
     }
     ros_utils::rosInfo("Active");
     if(isArmadilo) {
-        robotican_hardware::ArmadilloRobot robot;
-        robot.registerInterfaces();
-        controller_manager::ControllerManager controllerManager(&robot);
+        if(robotType == "default") {
+            robotican_hardware::ArmadilloRobot robot;
+            robot.registerInterfaces();
+            controller_manager::ControllerManager controllerManager(&robot);
 
-        ros::AsyncSpinner asyncSpinner(1);
-        asyncSpinner.start();
-        while(ros::ok()) {
-            robot.read();
-            controllerManager.update(robot.getTime(), robot.getPeriod());
-            robot.write();
+            ros::AsyncSpinner asyncSpinner(1);
+            asyncSpinner.start();
+            while (ros::ok()) {
+                robot.read();
+                controllerManager.update(robot.getTime(), robot.getPeriod());
+                robot.write();
 
+            }
         }
     }
     else if(isLizi) {
-        robotican_hardware::LiziRobot robot;
-        robot.registerInterfaces();
-        controller_manager::ControllerManager controllerManager(&robot);
-        ros::AsyncSpinner asyncSpinner(1);
-        asyncSpinner.start();
-        ros::Rate rate(20);
-        while(ros::ok()) {
-            robot.read();
-            controllerManager.update(robot.getTime(), robot.getPeriod());
-            robot.write();
-            rate.sleep();
+        if(robotType == "default") {
+            robotican_hardware::LiziRobot robot;
+            robot.registerInterfaces();
+            controller_manager::ControllerManager controllerManager(&robot);
+            ros::AsyncSpinner asyncSpinner(1);
+            asyncSpinner.start();
+            ros::Rate rate(20);
+            while (ros::ok()) {
+                robot.read();
+                controllerManager.update(robot.getTime(), robot.getPeriod());
+                robot.write();
+                rate.sleep();
 
+            }
         }
     }
     else if(isKomodo) {
-        robotican_hardware::KomodoRobot robot;
-        robot.registerInterfaces();
-        controller_manager::ControllerManager controllerManager(&robot);
-        ros::AsyncSpinner asyncSpinner(1);
-        asyncSpinner.start();
-        while(ros::ok()) {
-            robot.read();
-            controllerManager.update(robot.getTime(), robot.getPeriod());
-            robot.write();
+        if(robotType == "default") {
+            robotican_hardware::KomodoRobot robot;
+            robot.registerInterfaces();
+            controller_manager::ControllerManager controllerManager(&robot);
+            ros::AsyncSpinner asyncSpinner(1);
+            asyncSpinner.start();
+            while (ros::ok()) {
+                robot.read();
+                controllerManager.update(robot.getTime(), robot.getPeriod());
+                robot.write();
 
+            }
         }
     }
     return 0;
