54d2970238184ca3f6c305c120cee15b379defb1
tom wiss <tom@robotican.net>
2016-04-24 17:15:35 +0300

    add ros-teleop-tools

--- robotican/installations/third_pkg_setup/setup.sh
+++ robotican/installations/third_pkg_setup/setup.sh
@@ -12,7 +12,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo -e "\e[34mInstalling additional resources...\e[0m"
 apt-get -y install chrony openssh-server expect espeak ros-indigo-joy ros-indigo-hector-slam ros-indigo-usb-cam ros-indigo-openni2* ros-indigo-hokuyo-node ros-indigo-dynamixel-motor python-pygame ros-indigo-yocs-velocity-smoother
-apt-get -y install ros-indigo-moveit-full ros-indigo-ueye-cam ros-indigo-gazebo-ros-pkgs ros-indigo-gazebo-ros-control ros-indigo-hector-gazebo ros-indigo-ros-controllers ros-indigo-gmapping ros-indigo-navigation ros-indigo-robot-localization ros-indigo-twist-mux ros-indigo-serial
+apt-get -y install ros-indigo-moveit-full ros-indigo-ueye-cam ros-indigo-gazebo-ros-pkgs ros-indigo-gazebo-ros-control ros-indigo-hector-gazebo ros-indigo-ros-controllers ros-indigo-gmapping ros-indigo-navigation ros-indigo-robot-localization ros-indigo-twist-mux ros-indigo-serial ros-indigo-teleop-tools
 echo -e "\e[34mSyncing clock...\e[0m"
 ntpdate ntp.ubuntu.com
 
