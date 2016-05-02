b97b519f17547631c9582afe1b3ae3ee9c734f2a
tom wiss <tom@robotican.net>
2016-05-02 15:37:29 +0300

    add joy teleop in armadillo

---
+++ robotican_armadillo/src/joy_increment_node.cpp
@@ -0,0 +1,119 @@
+//
+// Created by tom on 02/05/16.
+//
+
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <sensor_msgs/JointState.h>
+#include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+
+typedef  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ActionClient;
+//#define DEBUG_ELEVETOR_INC
+class JoyIncrement {
+private:
+    ros::NodeHandle _nodeHandle;
+    ActionClient _actionClient;
+    ros::Subscriber _joySub;
+    ros::Subscriber _jointStates;
+
+    bool _isStop;
+    float _incElev;
+    double _elevPos;
+    int _upButtonIndex;
+    int _downButtonIndex;
+    int _deadManButtomIndex;
+
+    void joyCallback(const sensor_msgs::Joy::ConstPtr &msg) {
+        bool isUpActive = (bool)msg->buttons[_upButtonIndex],
+             isDownActive = (bool)msg->buttons[_downButtonIndex],
+             isDeadManButtonActive = (bool)msg->buttons[_deadManButtomIndex];
+        if(isDeadManButtonActive) {
+            if (isUpActive) {
+                sendGoal(_elevPos + _incElev);
+                _isStop = false;
+            }
+            else if (isDownActive) {
+                sendGoal(_elevPos - _incElev);
+                _isStop = false;
+            }
+            else if(!_isStop){
+                sendGoal(_elevPos);
+                _isStop = true;
+            }
+        }
+        else {
+            if(!_isStop) {
+                sendGoal(_elevPos);
+                _isStop = true;
+            }
+        }
+
+    }
+
+    void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg){
+        size_t size = msg->name.size();
+        bool found = false;
+        for(int i = 0; i < size && !found; ++i) {
+            if(msg->name[i] == "base_elevator_joint") {
+                _elevPos = msg->position[i];
+                found = true;
+            }
+        }
+        if(!found) {
+            ROS_ERROR("[%s]: base_elevator_joint not found", ros::this_node::getName().c_str());
+        }
+    }
+
+    void doneCallback(const actionlib::SimpleClientGoalState &state, const control_msgs::FollowJointTrajectoryResult::ConstPtr &result) {
+#ifdef DEBUG_ELEVETOR_INC
+        ROS_INFO("Finished in state [%s]", state.toString().c_str());
+#endif
+    }
+    void sendGoal(double elevPos) {
+//        ROS_INFO("[%s]: %f", ros::this_node::getName().c_str(), elevPos);
+        control_msgs::FollowJointTrajectoryGoal goal;
+        goal.goal_time_tolerance = ros::Duration(5.0);
+        goal.trajectory.header.frame_id = "base_link";
+        goal.trajectory.joint_names.push_back("base_elevator_joint");
+        trajectory_msgs::JointTrajectoryPoint point;
+        point.time_from_start = ros::Duration(5.0);
+        point.positions.push_back(elevPos);
+        goal.trajectory.points.push_back(point);
+        _actionClient.sendGoal(goal, boost::bind(&JoyIncrement::doneCallback, this, _1, _2),
+                               ActionClient::SimpleActiveCallback(), ActionClient::SimpleFeedbackCallback());
+    }
+
+
+public:
+    JoyIncrement() : _actionClient("elevator_trajectory_controller/follow_joint_trajectory", true) {
+        _isStop = true;
+        if(_actionClient.waitForServer(ros::Duration(20.0))) {
+            if(!_nodeHandle.getParam("elevetor_increment", _incElev)
+               ||!_nodeHandle.getParam("elevetor_up_button", _upButtonIndex)
+               ||!_nodeHandle.getParam("elevetor_down_button", _downButtonIndex)
+               ||!_nodeHandle.getParam("joy_deadman_button", _deadManButtomIndex)) {
+                ROS_ERROR("[%s]: Missing parameters, the requird parameters are: elevetor_increment,  elevetor_up_button, elevetor_down_button, joy_deadman_button", ros::this_node::getName().c_str());
+                ros::shutdown();
+            }
+            else {
+                _joySub = _nodeHandle.subscribe<sensor_msgs::Joy>("joy", 10, &JoyIncrement::joyCallback, this);
+                _jointStates = _nodeHandle.subscribe<sensor_msgs::JointState>("joint_states", 10, &JoyIncrement::jointStateCallback, this);
+            }
+        }
+        else {
+            ROS_ERROR("[%s]: Server not responding", ros::this_node::getName().c_str());
+            ros::shutdown();
+        }
+    }
+    void run() {
+        ros::spin();
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "joy_increment_node");
+    JoyIncrement joyIncrement;
+    joyIncrement.run();
+    return 0;
+}
---
+++ robotican_armadillo/src/pan_tilt_joy_node.cpp
@@ -0,0 +1,112 @@
+//
+// Created by tom on 02/05/16.
+//
+
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/Float64MultiArray.h>
+
+//#define DEBUG_JOY
+
+class PanTiltJoy {
+private:
+    int _panAxisIndex;
+    int _tiltAxisIndex;
+    float _deadManButtonIndex;
+    float _zeroButtonIndex;
+
+    float _incTilt;
+    float _incPan;
+
+    float _tiltPos;
+    float _panPos;
+    bool _deadManButtonActive;
+    bool _zeroButtonActive;
+
+    ros::NodeHandle _nodeHandle;
+    ros::Publisher _panTiltCommand;
+    ros::Subscriber _joySub;
+
+    void joyCallback(const sensor_msgs::Joy::ConstPtr & msg) {
+        _deadManButtonActive = msg->buttons[_deadManButtonIndex] == 1;
+        _zeroButtonActive = msg->buttons[_zeroButtonIndex] == 1;
+        if(_deadManButtonActive) {
+            if(msg->axes[_tiltAxisIndex] > 0) {
+                _tiltPos -= _incTilt;
+            }
+            else if(msg->axes[_tiltAxisIndex] < 0) {
+                _tiltPos += _incTilt;
+            }
+            if(msg->axes[_panAxisIndex] > 0) {
+                _panPos += _incPan;
+            }
+            else if(msg->axes[_panAxisIndex] < 0) {
+                _panPos -= _incPan;
+            }
+            if(_zeroButtonActive) {
+                _panPos = _tiltPos = 0.0;
+            }
+
+        }
+    }
+
+public:
+    PanTiltJoy() {
+        std::string panTiltTopic, joySubTopic;
+        _tiltPos = _panPos = 0;
+        _deadManButtonActive = _zeroButtonActive = false;
+
+        // Validation check.
+        if(!_nodeHandle.getParam("pan_tilt_topic", panTiltTopic)
+           || !_nodeHandle.getParam("joy_sub_topic", joySubTopic)
+           || !_nodeHandle.getParam("joy_pan_axis", _panAxisIndex)
+           || !_nodeHandle.getParam("joy_tilt_axis", _tiltAxisIndex)
+           || !_nodeHandle.getParam("increament_tilt", _incTilt)
+           || !_nodeHandle.getParam("increament_pan", _incPan)
+           || !_nodeHandle.getParam("joy_deadman_button", _deadManButtonIndex)
+           || !_nodeHandle.getParam("zero_button", _zeroButtonIndex)) {
+            ROS_ERROR("[%s]: Requird: pan_tilt_topic,  joy_sub_topic, joy_pan_axis, joy_tilt_axis, increament_tilt, increament_pan , zero_button, joy_deadman_button parameters", ros::this_node::getName().c_str());
+            ros::shutdown();
+        }
+        else {
+            _panTiltCommand = _nodeHandle.advertise<std_msgs::Float64MultiArray>(panTiltTopic, 10);
+            _joySub = _nodeHandle.subscribe<sensor_msgs::Joy>(joySubTopic, 10, &PanTiltJoy::joyCallback, this);
+        }
+    }
+
+    // Method which publish position to the pan_tilt.
+    void run() {
+        ros::Rate loopRate(50);
+        std_msgs::Float64MultiArray positions;
+        while(ros::ok()) {
+
+            if(_deadManButtonActive) {
+                if(positions.data.size() < 2) {
+                    positions.data.push_back(_panPos);
+                    positions.data.push_back(_tiltPos);
+                }
+                else {
+                    positions.data[0] = _panPos;
+                    positions.data[1] = _tiltPos;
+                }
+#ifdef DEBUG_JOY
+                ROS_INFO_STREAM("[" << ros::this_node::getName() << "]: " << positions);
+#endif
+                _panTiltCommand.publish(positions);
+            }
+
+            ros::spinOnce();
+            loopRate.sleep();
+        }
+    }
+
+
+
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "pan_tilt_joy_node");
+    PanTiltJoy panTiltJoy;
+    panTiltJoy.run();
+    return 0;
+}
