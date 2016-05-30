fc39f1202f0bd6daf706fef7c21dbd83c7590983
tom wiss <tom@robotican.net>
2016-05-30 14:55:24 +0300

    add clib imu

--- robotican_hardware_interface/include/robotican_hardware_interface/Imu.h
+++ robotican_hardware_interface/include/robotican_hardware_interface/Imu.h
@@ -9,20 +9,28 @@
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/MagneticField.h>
 #include <robotican_hardware_interface/Device.h>
+#include <robotican_hardware_interface/imuClib.h>
 #include <robotican_hardware_interface/ros_utils.h>
+#include <robotican_hardware_interface/setImuClib.h>
 #include <robotican_hardware_interface/RiCBoardManager.h>
 
 namespace robotican_hardware {
     class Imu : public Device {
     private:
+        byte _imuState;
         uint16_t _fusionHz;
         std::string _frameId;
-        bool _enableGyro;
-
+        ros::ServiceServer _setImuClibService;
+        ros::Publisher _clibPub;
         ros::Publisher _imuAMQ;
         ros::Publisher _imuM;
+        bool _enableGyro;
+        bool _isStopClib;
+        bool _isStateChange;
 
+        bool onSetImuClib(robotican_hardware_interface::setImuClib::Request &request, robotican_hardware_interface::setImuClib::Response &response);
     public:
+
         Imu(byte id, TransportLayer *transportLayer, uint16_t fusionHz, std::string frameId, bool enableGyro);
 
         virtual void update(const DeviceMessage *deviceMessage);
@@ -32,7 +40,6 @@ namespace robotican_hardware {
         virtual void buildDevice();
 
         virtual void deviceAck(const DeviceAck *ack);
-
     };
 }
 
