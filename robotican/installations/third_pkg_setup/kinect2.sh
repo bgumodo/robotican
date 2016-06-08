1603aff6482a606049412fc6a77c2cf3bd931526
tom wiss <tom@robotican.net>
2016-06-08 16:56:15 +0300

    update kinect2.sh add -y in apt-get

--- robotican/installations/third_pkg_setup/kinect2.sh
+++ robotican/installations/third_pkg_setup/kinect2.sh
@@ -5,13 +5,13 @@ sudo chown -R $(logname):$(logname) ~/libfreenect2
 
 cd libfreenect2
 cd depends; ./download_debs_trusty.sh
-sudo apt-get install build-essential cmake pkg-config
+sudo apt-get -y install build-essential cmake pkg-config
 sudo dpkg -i debs/libusb*deb
-sudo apt-get install libturbojpeg libjpeg-turbo8-dev
-sudo dpkg -i debs/libglfw3*deb; sudo apt-get install -f
-sudo apt-add-repository ppa:floe/beignet; sudo apt-get update; sudo apt-get install beignet-dev; sudo dpkg -i debs/ocl-icd*deb
-sudo dpkg -i debs/{libva,i965}*deb; sudo apt-get install -f
-sudo apt-get install libopenni2-dev
+sudo apt-get -y install libturbojpeg libjpeg-turbo8-dev
+sudo dpkg -i debs/libglfw3*deb; sudo apt-get -y install -f
+sudo apt-add-repository ppa:floe/beignet; sudo apt-get update; sudo apt-get -y  install beignet-dev; sudo dpkg -i debs/ocl-icd*deb
+sudo dpkg -i debs/{libva,i965}*deb; sudo -y apt-get install -f
+sudo apt-get -y  install libopenni2-dev
 cd ..
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/freenect2 -DENABLE_CXX11=ON
